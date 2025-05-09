9e4da5b9829ad83fa952bb5b6970ed20855a4f42
Danish Khan <khandanishe125@gmail.com>
2025-05-09 14:03:05 +0530

    1. some progress

---
+++ JavaBasic.java
@@ -0,0 +1,51 @@
+// public class JavaBasic{
+//   public static void main(String args[]){
+//     This is the boilerplate Code
+//   }
+// }
+
+public class JavaBasic {
+  public static void main(String args[]) {
+
+    // [output print format]
+    // System.out.println("hello world");
+    // System.out.print("hello world\n");
+
+    // [pattern print]
+    // System.out.println(" ****\n ***\n **\n *");
+
+    // [variable declaration]
+    // int a = 10;
+    // int b = 20;
+    // String name = "danish khan";
+    // byte byteValue = 127;
+    // short shortValue = 32767;
+    // int intValue = 2147483647;
+    // long longValue = 9223372036854775807L;
+    // float floatValue = 3.14f;
+    // double doubleValue = 3.14159265359;
+    // char charValue = 'A';
+    // boolean booleanValue = true;
+    // String stringValue = "Hello Java";
+    // int[] arrayValue = { 1, 2, 3, 4, 5 };
+    // Integer wrapperInt = 100;
+
+    // System.out.println(name);
+    // System.out.println("byte: " + byteValue);
+    // System.out.println(a + b);
+    // System.out.println("short: " + shortValue);
+    // System.out.println("int: " + intValue);
+    // System.out.println("long: " + longValue);
+    // System.out.println("float: " + floatValue);
+    // System.out.println("double: " + doubleValue);
+    // System.out.println("char: " + charValue);
+    // System.out.println("boolean: " + booleanValue);
+    // System.out.println("String: " + stringValue);
+    // System.out.print("Array: ");
+    // for (int i = 0; i < arrayValue.length; i++) {
+    //   System.out.print(arrayValue[i] + " ");
+    // }
+    // System.out.println("\nWrapper class: " + wrapperInt);
+
+  }
+}
