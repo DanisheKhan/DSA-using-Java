516f96bcb05d5fbfdd72fb781cb1eb42a8438170
Danish Khan <khandanishe125@gmail.com>
2025-07-14 11:37:48 +0530

    feat: add implementations for maximum subarray sum, subarray pairs, and print subarrays

---
+++ Array/NineMaxSubarraySum.java
@@ -0,0 +1,33 @@
+package Array;
+
+import java.util.*;
+
+public class NineMaxSubarraySum {
+
+  public static void PS(int num[]) {
+    int currSum = 0;
+    int maxSum = Integer.MIN_VALUE;
+
+    for (int i = 0; i < num.length; i++) {  // Start from 0 instead of 1
+      for (int j = i; j < num.length; j++) {
+        currSum = 0;
+        System.out.print("Subarray [" + i + "..." + j + "]: ");
+        for (int k = i; k <= j; k++) {
+          currSum += num[k];
+          System.out.print(num[k] + " ");
+        }
+        if (maxSum < currSum) {
+          maxSum = currSum;
+        }
+        System.out.println(" Sum: " + currSum);
+      }
+      System.out.println();
+    }
+    System.out.println("Maximum Subarray Sum: " + maxSum);
+  };
+
+  public static void main(String[] args) {
+    int num[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+    PS(num);
+  }
+}
---
+++ Array/SevenPairsInArray.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class SevenPairsInArray {
+
+  public static void PP(int num[]) {
+    for (int i = 0; i < num.length; i++) {
+      for (int j = i + 1; j < num.length; j++) {
+        System.out.print("(" + num[i] + "," + num[j] + ") ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    int num[]={1,2,3,4,5,6,7,8};
+    
+    PP(num);
+  }
+
+}
