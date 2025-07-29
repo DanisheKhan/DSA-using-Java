5350147d81cd7bbef9caf91ed4bbce3e8d3beb38
Danish Khan <khandanishe125@gmail.com>
2025-07-29 23:31:05 +0530

    feat: add implementations for abstract classes and interfaces in Java OOP

---
+++ SevenOOPs/FifteenSuperKeyword.java
@@ -0,0 +1,21 @@
+package SevenOOPs;
+
+public class FifteenSuperKeyword {
+  public static void main(String[] args) {
+    Dog d = new Dog();
+    d.printColor();
+  }
+}
+
+class Animal {
+  String color = "brown";
+}
+
+class Dog extends Animal {
+  String color = "black";
+
+  void printColor() {
+    System.out.println("Dog color: " + color); // black (current class)
+    System.out.println("Animal color: " + super.color); // brown (parent class)
+  }
+}
