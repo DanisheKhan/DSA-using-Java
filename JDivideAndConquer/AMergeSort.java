720f92bb63e1c28509df1bfc7d3c054f4421e5f5
Danish Khan <khandanishe125@gmail.com>
2025-08-16 01:02:32 +0530

    feat: implement AMergeSort class with merge sort algorithm and array printing functionality

---
+++ JDivideAndConquer/AMergeSort.java
@@ -0,0 +1,50 @@
+package JDivideAndConquer;
+
+public class AMergeSort {
+  public static void printArray(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  public static void mergeSort(int arr[], int si, int ei) {
+    if (si >= ei) {
+      return;
+    }
+    int mid = si + (ei - si) / 2;
+    mergeSort(arr, si, mid);
+    mergeSort(arr, mid + 1, ei);
+    merge(arr, si, mid, ei);
+  }
+
+  public static void merge(int arr[], int si, int mid, int ei) {
+    int temp[] = new int[ei - si + 1];
+    int i = si;      // pointer for left part
+    int j = mid + 1; // pointer for right part
+    int k = 0;       // pointer for temp array
+
+    while (i <= mid && j <= ei) {
+      if (arr[i] <= arr[j]) {
+        temp[k++] = arr[i++];
+      } else {
+        temp[k++] = arr[j++];
+      }
+    }
+    while (i <= mid) {
+      temp[k++] = arr[i++];
+    }
+    while (j <= ei) {
+      temp[k++] = arr[j++];
+    }
+    for (k = 0; k < temp.length; k++) {
+      arr[si + k] = temp[k];
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] arr = { 38, 27, 43, 3, 9, 82, 10 };
+    mergeSort(arr, 0, arr.length - 1);
+    printArray(arr);
+  }
+}
